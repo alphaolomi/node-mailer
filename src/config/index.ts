36667ed970e17f75e3bbbe1ee791e09d2bc0a66e
Alpha Olomi <alphaolomi@gmail.com>
2020-05-04 10:51:59 +0300

    feat: add mailgun
    closes #2

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,3 +7,7 @@ export const ETHEREAL_PORT = process.env.ETHEREAL_PORT
 export const ETHEREAL_SECURITY = process.env.ETHEREAL_SECURITY
 export const ETHEREAL_USERNAME = process.env.ETHEREAL_USERNAME
 export const ETHEREAL_PASSWORD = process.env.ETHEREAL_PASSWORD
+
+
+export const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY || ''
+export const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || ''
